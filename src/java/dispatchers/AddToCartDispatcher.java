c99b750d1f0f207fc4de00387e13a90c27787361
001179998 <christian.castillo@student.tafesa.edu.au>
2025-06-14 20:41:00 +0930

    Generate Java DOC

--- src/java/dispatchers/AddToCartDispatcher.java
+++ src/java/dispatchers/AddToCartDispatcher.java
@@ -25,50 +25,6 @@ public class AddToCartDispatcher implements Dispatcher{
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) {
         
-//        HttpSession session = request.getSession(true);
-//        
-//         // Retrieve the cart from the session
-//            Map<String, CartItem> cart = (Map<String, CartItem>) session.getAttribute("cart");
-//            String[] selectedBooks = request.getParameterValues("add");
-//
-//            // Check if selectedBooks is null or empty
-//            if (selectedBooks == null || selectedBooks.length == 0) {
-////                this.dispatch(request, response, nextPage);
-//                return "/jsp/titles.jsp";
-//            }
-//
-//            // If the cart is null, create a new cart and add selected books
-//            if (cart == null) {
-//                cart = new HashMap();
-//
-//                for (String isbn : selectedBooks) {                   int quantity = Integer.parseInt(request.getParameter(isbn));
-////                    
-////                    Book book = this.getBookFromList(isbn, session);
-////                    CartItem item = new CartItem(book);
-////                    item.setQuantity(quantity);
-////                    cart.put(isbn, item);
-//                    if (cart.containsKey(isbn)) {
-//                        CartItem item = cart.get(isbn);
-//                        item.setQuantity(quantity);
-//                    } else {
-//                        //
-//                        Book book = getBookFromList(isbn, session);
-//                        CartItem item = new CartItem(book);
-//                        item.setQuantity(quantity);
-//                        cart.put(isbn, item);
-//                    }
-//                }
-//                session.setAttribute("cart", cart);
-//                return "/jsp/titles.jsp";
-//            } 
-//            
-//            
-//
-//            //this.dispatch(request, response, nextPage);
-//        return null;
-        
-        
-        
         HttpSession session = request.getSession(true);
         Map<String, CartItem> cart = (Map<String, CartItem>) session.getAttribute("cart");
         String[] selectedBooks = request.getParameterValues("add");
@@ -120,15 +76,8 @@ public class AddToCartDispatcher implements Dispatcher{
         return null;
     }
     
-//    private Book getBookFromList(String isbn, HttpSession session) {
-//        List<Book> list = (List<Book>) session.getAttribute("books");
-//        Book aBook = null;
-//        for (Book book : list) {
-//            if (isbn.equals(book.getIsbn())) {
-//                return book;
-//            }
-//        }
-//        return null;
-//    }
-    
 }
+
+
+
+
--- src/java/dispatchers/TitleDispatcher.java
+++ src/java/dispatchers/TitleDispatcher.java
@@ -51,34 +51,6 @@ public class TitleDispatcher implements Dispatcher{
             return "/jsp/error.jsp";
         }
         
-        
-        
-         
-        
-//        HttpSession session = request.getSession();
-//        String nextPage = "/jsp/error.jsp";
-//
-//        try {
-//            // Get the robot librarian (EntityManager)
-//            EntityManagerFactory emf = Persistence.createEntityManagerFactory("BookShopPU");
-//            EntityManager em = emf.createEntityManager();
-//
-//            // Ask for all the books
-//            TypedQuery<Tbooks> query = em.createNamedQuery("Tbooks.findAll", Tbooks.class);
-//            List<Tbooks> books = query.getResultList();
-//
-//            // Save books in the session for JSP to use
-//            session.setAttribute("books", books);
-//            nextPage = "/jsp/titles.jsp";
-//
-//            em.close();
-//            emf.close();
-//
-//        } catch (Exception ex) {
-//            request.setAttribute("result", ex.getMessage());
-//        }
-//
-//        return nextPage;
     }
         
          public void persist(Object object) {
@@ -92,111 +64,38 @@ public class TitleDispatcher implements Dispatcher{
         }
     }
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-    }
-        
-//    @PersistenceContext(unitName = "BookShopPU")
-//    private EntityManager em;
-//    private EntityManagerFactory emf;
-//    @Resource
-//    private javax.transaction.UserTransaction utx;
-//        
-//        
-//        HttpSession session = request.getSession();
-//        AdmitBookStoreDAO dao = new AdmitBookStoreDAO();
-//        
-//        
-////        dao = new AdmitBookStoreDAO();
-////            List<Book> books = null;
-////            nextPage = "/jsp/error.jsp";
-////            session = request.getSession();
-////            try {
-////                books = dao.getAllBooks();
-////                //
-////                session.setAttribute("books", books);
-////                nextPage = "/jsp/titles.jsp";
-////            } catch (Exception ex) {
-////                request.setAttribute("result", ex.getMessage());
-////                nextPage = "/jsp/error.jsp";
-////            } finally {
-////                this.dispatch(request, response, nextPage);
-////            }
-//        String nextPage = "/jsp/error.jsp";
-//
-//        try {
-//            List<Tbooks> books = dao.getAllBooks();
-//            session.setAttribute("books", books);
-//            nextPage = "/jsp/titles.jsp";
-//        } catch (Exception ex) {
-//            request.setAttribute("result", ex.getMessage());
-//        }
-//
-//        return nextPage;
-//    }
-//
-//    public void persist(Object object) {
-//        /* Add this to the deployment descriptor of this module (e.g. web.xml, ejb-jar.xml):
-//         * <persistence-context-ref>
-//         * <persistence-context-ref-name>persistence/LogicalName</persistence-context-ref-name>
-//         * <persistence-unit-name>BookShopPU</persistence-unit-name>
-//         * </persistence-context-ref>
-//         * <resource-ref>
-//         * <res-ref-name>UserTransaction</res-ref-name>
-//         * <res-type>javax.transaction.UserTransaction</res-type>
-//         * <res-auth>Container</res-auth>
-//         * </resource-ref> */
-//        try {
-//            Context ctx = new InitialContext();
-//            UserTransaction utx = (UserTransaction) ctx.lookup("java:comp/env/UserTransaction");
-//            utx.begin();
-//            EntityManager em = (EntityManager) ctx.lookup("java:comp/env/persistence/LogicalName");
-//            em.persist(object);
-//            utx.commit();
-//        } catch (Exception e) {
-//            Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", e);
-//            throw new RuntimeException(e);
-//        }
-//    }
-//
-//    public void persist1(Object object) {
-//        /* Add this to the deployment descriptor of this module (e.g. web.xml, ejb-jar.xml):
-//         * <persistence-context-ref>
-//         * <persistence-context-ref-name>persistence/LogicalName</persistence-context-ref-name>
-//         * <persistence-unit-name>BookShopPU</persistence-unit-name>
-//         * </persistence-context-ref>
-//         * <resource-ref>
-//         * <res-ref-name>UserTransaction</res-ref-name>
-//         * <res-type>javax.transaction.UserTransaction</res-type>
-//         * <res-auth>Container</res-auth>
-//         * </resource-ref> */
-//        try {
-//            Context ctx = new InitialContext();
-//            UserTransaction utx = (UserTransaction) ctx.lookup("java:comp/env/UserTransaction");
-//            utx.begin();
-//            EntityManager em = (EntityManager) ctx.lookup("java:comp/env/persistence/LogicalName");
-//            em.persist(object);
-//            utx.commit();
-//        } catch (Exception e) {
-//            Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", e);
-//            throw new RuntimeException(e);
-//        }
-//    }
-    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
